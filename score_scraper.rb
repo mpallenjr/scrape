c6b85bc7a90491fefe124a8bfd00aa92db4ed9f5
Mike Allen <mpallenjnr@gmail.com>
2024-09-30 09:16:03 -0500

    update api url

--- score_scraper.rb
+++ score_scraper.rb
@@ -1,17 +1,5 @@
 require 'curb'
 require 'json'
 
-http = Curl.get("https://api-secure.sports.yahoo.com/v1/editorial/s/scoreboard?lang=en-US&region=US&tz=America%2FChicago&ysp_redesign=1&ysp_platform=desktop&game_ids=mlb.g.440929126")
-# puts http.body
-
-json_response = JSON.parse(http.body)
-puts json_response["service"]["scoreboard"]["games"]["mlb.g.440929126"]["total_away_points"]
-
-
-def get_cardinals_score(game_id)
-
-end
-
-def get_cardinals_game_id
-  
-end
+http = Curl.get("https://sportsdata.usatoday.com/baseball/mlb/scores?date=2024-09-28&season=2024")
+puts http.body
